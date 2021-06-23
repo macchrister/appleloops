b6128ea525160e271466d6432577596789cbf21e
carlashley <git@macapakr.com>
2021-06-23 18:38:59 +1000

    Resolved packages missing from comparison output and added html output

--- src/loopslib/compare.py
+++ src/loopslib/compare.py
@@ -2,6 +2,8 @@ import difflib
 import logging
 import sys
 
+from pathlib import Path
+
 from . import disk
 from . import source
 from . import ARGS
@@ -11,14 +13,14 @@ LOG = logging.getLogger(__name__)
 
 def sources(plist_a, plist_b, style=ARGS.compare_style):
     """Compares packages in the specified sources."""
-    styles = ['compare', 'unified']
+    styles = ['context', 'unified', 'html']
 
     if style not in styles:
         LOG.info('Invalid diff style selected. Choose from {styles}'.format(styles=styles))
         sys.exit(88)
 
-    packages_a = source.PropertyList(plist=plist_a).packages
-    packages_b = source.PropertyList(plist=plist_b).packages
+    packages_a = source.PropertyList(plist=plist_a, comparing=True).packages
+    packages_b = source.PropertyList(plist=plist_b, comparing=True).packages
 
     if packages_a:
         _unsequenced_a = sorted([pkg for pkg in packages_a if not pkg.sequence_number], key=lambda pkg: pkg.download_name)
@@ -39,14 +41,25 @@ def sources(plist_a, plist_b, style=ARGS.compare_style):
                                                                        mandatory='Mandatory' if pkg.mandatory else 'Optional',
                                                                        size=disk.convert(pkg.download_size)) for pkg in packages_b]
 
-        if style == 'compare':
-            differ = difflib.Differ()
-            diff = list(differ.compare(packages_a_strings, packages_b_strings))
+        if style == 'context':
+            diff = difflib.context_diff(packages_a_strings, packages_b_strings)
         elif style == 'unified':
             diff = difflib.unified_diff(packages_a_strings, packages_b_strings, fromfile=plist_a, tofile=plist_b)
+        elif style == 'html':
+            output = Path().home() / 'Desktop/{plist1}_{plist2}.html'.format(plist1=plist_a.replace('.plist', ''),
+                                                                             plist2=plist_b.replace('.plist', ''))
+            differ = difflib.HtmlDiff()
+            diff = differ.make_file(packages_a_strings, packages_b_strings, fromdesc=plist_a, todesc=plist_b)
 
-        for line in diff:
-            print(line.strip('\n'))
+            with open(output, 'w') as _f:
+                _f.write(diff)
+
+            if output.exists():
+                LOG.info('Diff HTML output to {output}'.format(output=output))
+
+        if style != 'html':
+            for line in diff:
+                print(line.strip('\n'))
 
         sys.exit(0)
     else:
