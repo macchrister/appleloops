c058a4a88d5dd3af1b253729c293382ea0e06156
carlashley <git@macapakr.com>
2021-07-02 18:25:00 +1000

    More parse functions and other improvements

--- src/loopslib/package.py
+++ src/loopslib/package.py
@@ -11,14 +11,14 @@ from . import pkgutil
 from . import versions
 from . import ARGS
 from . import DMG_MOUNT
+from . import FAIL_LOG
 from . import FEED_URL
-from . import PKG_SERVER_IS_DMG
+from . import INSTALL_TARGET
 from . import RUN_UUID
 
-messages.logging_conf(log_name='failedinstalls', silent=True, level='WARNING', log_file='appleloops_install_fails.log')
-
+PKG_SERVER_IS_DMG = True if ARGS.pkg_server and str(ARGS.pkg_server).endswith('.dmg') else False
 LOG = logging.getLogger(__name__)
-FAILED = logging.getLogger('failedinstalls')
+FAILED = messages.logging_conf(log_name='failedinstalls', silent=True, level='WARNING', log_file=FAIL_LOG)
 FAILED.warning('Run UUID: {uuid}'.format(uuid=RUN_UUID))
 FAILED.warning(ARGS)
 
@@ -48,16 +48,16 @@ class LoopPackage:
         self.upgrade = pkgutil.upgrade_pkg(self.installed_version, self.version)
         self.url = self.parse_url(self.download_name)
         self.download_dest = self.parse_dest(self.url)
-        self.download_size = curl.headers(self.url).get('content-length', 0)
+        self.download_size = self.parse_download_size(self.url)
         self.badwolf_ignore = False
-        self.status = curl.status(self.url)
+        self.status = self.parse_http_status(self.url)
         self.download_name = str(PurePath(self.download_name).name)  # Make the download name friendly
         self.sequence_number = self.parse_seq_number(self.download_name)
 
         # Set installed/upgrade values if force is required
         if ARGS.force:
             self.installed = False
-            self.upgrade = True
+            self.upgrade = False
 
         # Add self.package_id to INSTANCES tracker
         self.__class__.INSTANCES.add(self.package_id)
@@ -89,6 +89,41 @@ class LoopPackage:
 
         return result
 
+    def parse_http_status(self, u):
+        """Parse the HTTP status, returns an int"""
+        result = 0
+
+        if ARGS.deployment:
+            if urlparse(u).scheme:
+                result = curl.status(u)
+            elif PKG_SERVER_IS_DMG:
+                if Path(u).exists():
+                    result = 200
+                else:
+                    result = 404
+
+                LOG.debug('Set HTTP status to {status} for local path'.format(status=result))
+        else:
+            result = curl.status(u)
+
+        return result
+
+    def parse_download_size(self, u):
+        """Parse the download size, returns an int"""
+        result = 0
+
+        if ARGS.deployment:
+            if urlparse(u).scheme:
+                result = curl.headers(self.url).get('content-length', 0)
+            elif PKG_SERVER_IS_DMG:
+                if Path(u).exists():
+                    result = Path(u).stat().st_size
+                    LOG.debug('Download size set to local path file size {size}'.format(size=result))
+        else:
+            result = curl.headers(self.url).get('content-length', 0)
+
+        return result
+
     def parse_seq_number(self, p):
         """Parse out a sequence number from the package name"""
         result = None
@@ -117,10 +152,18 @@ class LoopPackage:
         # Change the URL if package server is specified
         if ARGS.pkg_server:
             if '.dmg' not in str(ARGS.pkg_server):
-                url = url.replace(FEED_URL, str(ARGS.pkg_server))
+                # Modifies the existing Apple url to the package server url by combining urlparse attributes
+                apple_server = urlparse(url)
+                pkg_server = urlparse(str(ARGS.pkg_server))
+                pkg_path = '{path}{apple_path}'.format(path=pkg_server.path, apple_path=apple_server.path)
+                url = '{scheme}://{netloc}{path}'.format(scheme=pkg_server.scheme, netloc=pkg_server.netloc, path=pkg_path)
             elif '.dmg' in str(ARGS.pkg_server):
                 LOG.debug('Swapping packge URL to download name attribute for DMG based deployment')
-                url = n
+                apple_server = urlparse(url)
+                if ARGS.flat_mirror:
+                    url = '{dmg_mount}{path}'.format(dmg_mount=DMG_MOUNT, path=PurePath(apple_server.path).name)
+                else:
+                    url = '{dmg_mount}{path}'.format(dmg_mount=DMG_MOUNT, path=apple_server.path)
 
         # Only log the URL update if the url differs
         if url != _url:
@@ -133,17 +176,25 @@ class LoopPackage:
     def parse_dest(self, u):
         """Parse a download destination."""
         result = None
+        # Setting destination here avoids issues where disk space checks would fail if
+        # downloading to a DMG as the sparseimage will have no space, but the drive
+        # the sparseimage is stored on will have the space.
+        destination = ARGS.build_dmg if ARGS.build_dmg else ARGS.destination
 
         if ARGS.flat_mirror:
-            dest = '{dest}/{pkgname}'.format(dest=ARGS.destination, pkgname=PurePath(u).name)
+            dest = '{dest}/{pkgname}'.format(dest=destination, pkgname=PurePath(u).name)
         else:
-            dest = '{dest}/{pkgname}'.format(dest=ARGS.destination, pkgname=urlparse(u).path.lstrip('/'))
+            dest = '{dest}/{pkgname}'.format(dest=destination, pkgname=urlparse(u).path.lstrip('/'))
 
-        if ARGS.deployment and ARGS.pkg_server and PKG_SERVER_IS_DMG:
-            if ARGS.flat_mirror:
-                dest = dest.replace(str(ARGS.destination), DMG_MOUNT).replace('lp10_ms3_content_2016/', '').replace('lp10_ms3_content_2013/', '')
-            else:
-                dest = dest.replace(str(ARGS.destination), DMG_MOUNT)
+        if ARGS.deployment:
+            if ARGS.pkg_server:
+                if PKG_SERVER_IS_DMG:
+                    dest = self.url
+
+                    if ARGS.flat_mirror:
+                        dest.replace(str(ARGS.destination), DMG_MOUNT).replace('lp10_ms3_content_2016/', '').replace('lp10_ms3_content_2013/', '')
+                else:
+                    dest = '/tmp/appleloops/{package}'.format(package=PurePath(u).name)
 
         result = Path(self._regex_parse_string(dest))
 
@@ -152,14 +203,14 @@ class LoopPackage:
     def install(self):
         """Install package, returns a tuple with the process returncode at [0] and success/error message at [1]"""
         result = None
-        cmd = ['/usr/sbin/installer', '-dumplog', '-pkg', str(self.download_dest), '-target', str(ARGS.install_target)]
+        cmd = ['/usr/sbin/installer', '-dumplog', '-pkg', str(self.download_dest), '-target', str(INSTALL_TARGET)]
 
         # Insert the untrusted flag into the correct part of the install command
         if ARGS.unsigned:
             cmd.insert(1, '-allowUntrusted')
 
         if not ARGS.dry_run:
-            LOG.debug(' '.join(cmd))
+            LOG.debug(' '.join([str(x) for x in cmd]))
             _pfx = 'Upgraded' if self.upgrade else 'Installed'
             _p = subprocess.run(cmd, capture_output=True, encoding='utf-8')
 
@@ -173,6 +224,7 @@ class LoopPackage:
                                                                                          pkg=self.package_id,
                                                                                          log=_lp))
                 LOG.debug(_p.stderr.strip())
+
                 FAILED.warning('{pkgname} - source: {pkgurl}'.format(pkgname=self.download_name,
                                                                      pkgurl=self.url))
                 FAILED.warning('{pkgname} - dest: {pkgdest}'.format(pkgname=self.download_name,
