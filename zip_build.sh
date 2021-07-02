91fd00d3c07ec84150497eeb4b499e91bf997f2a
carlashley <git@macapakr.com>
2021-07-02 18:29:11 +1000

    Purge pycache before build

--- zip_build.sh
+++ zip_build.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
 
+/usr/bin/find . -type d -iname __pycache__ -exec rm -r {} \; 2>/dev/null
+
 # /usr/local/bin/python3 -m zipapp src --compress --output appleloops --python="/usr/local/bin/python3"
 /usr/local/bin/python3 -m zipapp src --output appleloops --python="/usr/local/bin/python3"
